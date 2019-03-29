e179586065d57b32e63716019f0db8c06aa2e1b0
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2019-03-29 13:13:53 -0300

    Bonus section, finished.

--- regression.py
+++ regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import r2_score
 from sklearn.ensemble import RandomForestRegressor
 
 # Regression

18d42a093d4e12689e4e01ab1b5da9378f8474d5
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2019-03-29 09:22:21 -0300

    Classification, finished

--- classification_naiveBayes.py
+++ classification_naiveBayes.py
@@ -3,22 +3,47 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-# Question 3
+# Naive Bayes
+# The objective of this module is to classify the data from the pre-processed dataset, 
+# based on the alignment of each hero (good or bad), using the Naive Bayes algorithm.
 
+# Question 3:
+#     1- The Naive Bayes algorithm is a classifier based on the Bayes' theorem, 
+#        which is based on conditional probability. It is a probabilistic algorithm that 
+#        returns the probability of each entry belonging to a class. Two assumptions are 
+#        made by this algorithm, the first is that the features are independent, and the second 
+#        is that all features have an equal effect on the classification. Because of this assumptions
+#        the algorithm is called 'Naive'.
+#
+#     2- Due to the large number of categorical variables and most of them having several categories, 
+#        it was decided to reduce the number of categories and to transform each category into a column 
+#        representing whether or not the hero has such a characteristic. Due to the same characteristics 
+#        of the data set it was chosen to use the Multinomial Naive Bayes algorithm, which has a better 
+#        performance with this type of data.
+#    
+#     3- Accuracy is being used as a metric to evaluate the model result,  
+
+
+# Read pre-processsed dataset
 df = pd.read_csv('datasets/info_power_processed.csv')
 
 X = df.drop(columns=['name', 'Alignment'])
 
+# Classifying in good or bad
 y = df['Alignment']
 
+# Split dataset into training set and test set # 70% training and 30% test
 X_train, X_test, y_train, y_test = train_test_split(X, y.values.ravel(), 
-                                                    test_size=0.3,random_state=109) # 70% training and 30% test
+                                                    test_size=0.3,random_state=109) 
 
+# Initialize algorithm
 clf = MultinomialNB()
 
+# Training model
 clf.fit(X_train, y_train)
 
+# Predicting test cases 
 y_pred = clf.predict(X_test)
 
-# Model Accuracy, how often is the classifier correct?
+# Model Accuracy
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
--- classification_randomForest.py
+++ classification_randomForest.py
@@ -4,31 +4,54 @@ from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-# Question 4
+# Random Forest
+# The objective of this module is to classify the data from the pre-processed dataset,
+#  based on the alignment of each hero (good or bad), using the Random Forest algorithm.
 
+# Question 4:
+
+#     1- It is a simple algorithm that has a good performance in most cases and can be 
+#        used without the use of hyperparametres. Moreover, due to its structure in 
+#        decision trees, it has the capacity to measure the relative importance of each 
+#        feature for the classification.
+#    
+#     2-  Comparing acuracy, the random forest algorithm had a slightly better result 
+#         than the naive bayes. Random forest also has differences in relation to assumptions. 
+#         While naive bayes consider that the variables are independent and of equal weight, 
+#         random forest uses several decision trees to assign weights to the features, and due 
+#         to the randomness of these trees the algorithm adds diversity to the process.
+
+# Read pre-processsed dataset
 df = pd.read_csv('datasets/info_power_processed.csv')
 
 X = df.drop(columns=['name', 'Alignment'])
 
+# Classifying in good or bad
 y = df['Alignment']
 
+# Split dataset into training set and test set # 70% training and 30% test
 X_train, X_test, y_train, y_test = train_test_split(X, y.values.ravel(), 
-                                                    test_size=0.3,random_state=109) # 70% training and 30% test
+                                                    test_size=0.3,random_state=109)
 
 
+# Initialize algorithm
 clf = RandomForestClassifier(n_estimators = 100, random_state=109)
 
+# Training model
 clf.fit(X_train, y_train)
 
+# Predicting test cases 
 y_pred = clf.predict(X_test)
   
+# Model Accuracy
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred),'\n')
 
+# Feature selection from model
 sfm = SelectFromModel(clf, threshold=0.05)
 
 # Train the selector
 sfm.fit(X_train, y_train)
 
 print('More important features selected:')
-for feature_list_index in sfm.get_support(indices=True):
-    print(X_train.columns[feature_list_index])
+for feat_index in sfm.get_support(indices=True):
+    print(X_train.columns[feat_index], clf.feature_importances_[feat_index])
